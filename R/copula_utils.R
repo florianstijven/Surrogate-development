07064025a5d539b83e7c87bb12377d2a6a428800
florianstijven <florian.stijven@gmail.com>
2023-04-17 19:08:25 +0200

    Add Gaussian copula likelihood function and corresponding documentation.

--- R/copula_utils.R
+++ R/copula_utils.R
@@ -34,12 +34,12 @@ clayton_loglik_copula_scale <- function(theta, u, v, d1, d2){
            (theta + 1) * log_C - (theta + 1) * u,
            0)
   # Log likelihood contribution for first observation censored.
-  part2 <-
-    ifelse(d1 * (1 - d2) == 1,
+  part3 <-
+    ifelse((1 - d1) * d2 == 1,
            (theta + 1) * log_C - (theta + 1) * v,
            0)
   # Log likelihood contribution for both observations censored.
-  part4 <- ifelse((1-d1)*(1-d2)==1,log_C,0)
+  part4 <- ifelse((1 - d1) * (1 - d2) == 1, log_C, 0)
 
   loglik_copula <- part1 + part2 + part3 + part4
 
@@ -81,7 +81,7 @@ frank_loglik_copula_scale <- function(theta, u, v, d1, d2){
            0)
   # Log likelihood contribution for first observation censored.
   part3 <-
-    ifelse(d1 * (1 - d2) == 1,
+    ifelse((1 - d1) * d2 == 1,
            log(1 - exp(theta * C) - log(A_v)),
            0)
   # Log likelihood contribution for both observations censored.
@@ -124,8 +124,8 @@ gumbel_loglik_copula_scale <- function(theta, u, v, d1, d2){
            log_C + (theta - 1) * log(min_log_u) + min_log_u - (theta - 1) * log(-log_C),
            0)
   # Log likelihood contribution for first observation censored.
-  part2 <-
-    ifelse(d1 * (1 - d2) == 1,
+  part3 <-
+    ifelse((1 - d1) * d2 == 1,
            log_C + (theta - 1) * log(min_log_v) + min_log_v - (theta - 1) * log(-log_C),
            0)
   # Log likelihood contribution for both observations censored.
@@ -136,138 +136,73 @@ gumbel_loglik_copula_scale <- function(theta, u, v, d1, d2){
   return(loglik_copula)
 }
 
-#' @importFrom stats pnorm qnorm
-normal_loglik <- function(para, X, Y, d1, d2, k = 2, knotsx, knotsy){
-  #k is the number of knots in the model, this determines the length of para
-  gammax <- para[1:(k + 2)]
-  gammay <- para[(k + 3):(2*(k + 2))]
-  #last value in para is rho
-  rho <- (exp(para[2*(k + 2) + 1]) - 1)/(exp(para[2*(k + 2) + 1]) + 1)
 
-  df.1 <- d1 & d2     #case part 1
-  df.2 <- d1 & (!d2)  #case part 2
-  df.3 <- (!d1)&d2;   #case part 3
-  df.4 <- (!d1)&(!d2) #case part 4
+#' Loglikelihood on the Copula Scale for the Gaussian Copula
+#'
+#' `normal_loglik_copula_scale()` computes the loglikelihood on the copula
+#' scale for the Gaussian copula which is parameterized by `theta` as follows:
+#' \deqn{C(u, v) = \Psi \left[ \Phi^{-1} (u), \Phi^{-1} (v) | \rho \right]}
+#'
+#' @inheritParams clayton_loglik_copula_scale
+#'
+#' @return Value of the copula loglikelihood evaluated in `theta`.
+normal_loglik_copula_scale <- function(theta, u, v, d1, d2){
+  # For efficiency purposes, some quantities that are needed multiple times are
+  # first precomputed. In this way, we do not waste resources on computing the
+  # same quantity multiple times.
 
-  df = data.frame(X, Y)
+  # Covariance matrix
+  Sigma = matrix(c(1, theta, 1, theta), nrow = 2)
 
-  X.1 <- df[df.1,1]
-  Y.1 <- df[df.1,2]
-  X.2 <- df[df.2,1]
-  Y.2 <- df[df.2,2]
-  X.3 <- df[df.3,1]
-  Y.3 <- df[df.3,2]
-  X.4 <- df[df.4,1]
-  Y.4 <- df[df.4,2]
+  # Transform the variables on the copula scale to the standard normal scale.
+  z_u = stats::qnorm(u)
+  z_v = stats::qnorm(v)
 
-  part1 <-
-    ifelse((sum(df.1) > 0), sum(
-      -0.5 * log(1 - rho ^ 2) + (((
-        2 * rho * qnorm(flexsurv::psurvspline(
-          q = X.1, gamma = gammax, knots = knotsx
-        )) * qnorm(flexsurv::psurvspline(
-          q = Y.1, gamma = gammay, knots = knotsy
-        )) -
-          rho ^ 2 * (qnorm(
-            flexsurv::psurvspline(q = X.1, gamma = gammax, knots = knotsx)
-          ) ^ 2 + qnorm(
-            flexsurv::psurvspline(q = Y.1, gamma = gammay, knots = knotsy)
-          ) ^ 2)
-      )) / ((2 * (
-        1 - rho ^ 2
-      ))))
-      + log(flexsurv::dsurvspline(
-        x = X.1, gamma = gammax, knots = knotsx
-      )) + log(flexsurv::dsurvspline(
-        x = Y.1, gamma = gammay, knots = knotsy
-      ))
-    ), 0)
-
-  part2 <-
-    ifelse((sum(df.2) > 0), sum(log(
-      pnorm(
-        qnorm(
-          flexsurv::psurvspline(
-            q = Y.2,
-            gamma = gammay,
-            knots = knotsy,
-            timescale = "log"
-          )
-        ),
-        mean = rho * qnorm(
-          flexsurv::psurvspline(
-            q = X.2,
-            gamma = gammax,
-            knots = knotsx,
-            timescale = "log"
-          )
-        ),
-        sd = sqrt(1 - rho ^ 2),
-        lower.tail = F
-      )
-    ) + log(
-      flexsurv::dsurvspline(
-        x = X.2,
-        gamma = gammax,
-        knots = knotsx,
-        timescale = "log"
-      )
-    )), 0)
-
-  part3 <-
-    ifelse((sum(df.3) > 0), sum(log(
-      pnorm(
-        qnorm(
-          flexsurv::psurvspline(
-            q = X.3,
-            gamma = gammax,
-            knots = knotsx,
-            timescale = "log"
-          )
-        ),
-        mean = rho * qnorm(
-          flexsurv::psurvspline(
-            q = Y.3,
-            gamma = gammay,
-            knots = knotsy,
-            timescale = "log"
-          )
-        ),
-        sd = sqrt(1 - rho ^ 2),
-        lower.tail = F
-      )
-    ) + log(
-      flexsurv::dsurvspline(
-        x = Y.3,
-        gamma = gammay,
-        knots = knotsy,
-        timescale = "log"
-      )
-    )), 0)
-
-  cov_matrix <- matrix(c(1,rho,rho,1),nrow=2)
-  normal_cdf <- function(V,sigma){
-    return(mvtnorm::pmvnorm(lower = V,upper=Inf, sigma=sigma,mean=c(0,0))[1])
-  }
-
-  part4 <- ifelse((sum(df.4) > 0), sum(log(apply(
-    qnorm(cbind(
-      flexsurv::psurvspline(
-        q = X.4,
-        gamma = gammax,
-        knots = knotsx,
-        timescale = "log"
+  # Merge the variables on the standard normal scale into a matrix with one
+  # column for each variable.
+  z_UV = matrix(c(z_u, z_v), byrow = FALSE, ncol = 2)
+  # Evaluate the Gaussian copula in (uv).
+  C = apply(
+    X = UV,
+    FUN = function(x)
+      mvtnorm::pmvnorm(
+        upper = x,
+        sigma = Sigma,
+        keepAttr = FALSE
       ),
-      flexsurv::psurvspline(
-        q = Y.4,
-        gamma = gammay,
-        knots = knotsy,
-        timescale = "log"
-      )
-    )), 1, normal_cdf, cov_matrix
-  ))), 0)
+    MARGIN = 2,
+    simplify = TRUE
+  )
 
-  loglik <- (part1+part2+part3+part4)
-  return(loglik)
+  # Log likelihood contribution for uncensored observations.
+  part1 <-
+    ifelse((1 - d1) * (1 - d2) == 1,
+           log(C) - dnorm(z_u, log = TRUE) - dnorm(z_v, log = TRUE),
+           0)
+  # Log likelihood contribution for second observation censored.
+  part2 <-
+    ifelse(d1 * (1 - d2) == 1,
+           pnorm(
+             q = (z_v - z_u * theta) / sqrt(1 - theta ** 2),
+             log.p = TRUE
+           ),
+           0)
+  # Log likelihood contribution for first observation censored.
+  part3 <-
+    ifelse((1 - d1) * d2 == 1,
+           pnorm(
+             q = (z_u - z_v * theta) / sqrt(1 - theta ** 2),
+             log.p = TRUE
+           ),
+           0)
+  # Log likelihood contribution for both observations censored.
+  part4 <-
+    ifelse((1 - d1) * (1 - d2) == 1,
+           log(C),
+           0)
+
+  loglik_copula <- part1 + part2 + part3 + part4
+
+  return(loglik_copula)
 }
 
