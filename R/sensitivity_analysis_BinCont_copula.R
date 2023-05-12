88420a0720f616b01db525ba141dc2b6ba5c49d0
florianstijven <florian.stijven@gmail.com>
2023-05-12 12:01:43 +0200

    Add skeleton for sensitivity analysis functions for the binary-continuous setting

---
+++ R/sensitivity_analysis_BinCont_copula.R
@@ -0,0 +1,20 @@
+sample_copula_parameters = function() {
+
+}
+
+sample_rotation_parameters = function() {
+
+}
+
+sensitivity_analysis_BinCont_copula = function(fitted_model) {
+  # Extract relevant estimated parameters/objects for the fitted copula model.
+
+  # Sample unidentifiable parameters.
+
+  # For every set of sampled unidentifiable parameters, compute the required
+  # quantities.
+
+
+  # Return a data frame with the results of the sensiviity analysis.
+
+}
