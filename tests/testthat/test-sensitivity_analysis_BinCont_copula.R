a3e7ae54e90104e4039d991e374ef9ebd9896363
florianstijven <florian.stijven@gmail.com>
2023-05-23 18:08:09 +0200

    Add test for sensitivity_analysis_BinCont_copula()

--- tests/testthat/test-sensitivity_analysis_BinCont_copula.R
+++ tests/testthat/test-sensitivity_analysis_BinCont_copula.R
@@ -1,3 +1,30 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+test_that("sensitivity_analysis_BinCont_copula() works in a simplified setting",
+          {
+            copula_family = "clayton"
+            marginal_surrogate = "normal"
+            data("Schizo_BinCont")
+            na = is.na(Schizo_BinCont$CGI_Bin) | is.na(Schizo_BinCont$PANSS)
+            X = Schizo_BinCont$PANSS[!na]
+            Y = Schizo_BinCont$CGI_Bin[!na]
+            Treat = Schizo_BinCont$Treat[!na]
+            Treat = ifelse(Treat == 1, 1, 0)
+            data = data.frame(X,
+                              Y,
+                              Treat)
+            fitted_model = fit_copula_model_BinCont(data, copula_family, marginal_surrogate, twostep = FALSE)
+
+            n_sim = 2
+            set.seed(1)
+            sens_results = sensitivity_analysis_BinCont_copula(
+              fitted_model,
+              n_sim,
+              cond_ind = TRUE,
+              lower = c(-1,-1,-1,-1),
+              upper = c(1, 1, 1, 1),
+              minfo_prec = 1e3
+            )
+            expect_equal(
+              sens_results[,1],
+              c(0.452785805, 0.294479978)
+            )
+          })
