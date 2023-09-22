9f1479d82908a6ff41b16f9a77923944d9bd59a6
florianstijven <florian.stijven@gmail.com>
2023-09-22 12:09:47 +0200

    Add update to the summary function of ICA.BinCont()

--- R/summary_ICABinCont.R
+++ R/summary_ICABinCont.R
@@ -1,9 +1,9 @@
 summary.ICA.BinCont <- function(object, ..., Object){
- 
+
   options(digits = 4)
-  
-  if (missing(Object)){Object <- object} 
-  
+
+  if (missing(Object)){Object <- object}
+
   Object$R2_H <- na.exclude(Object$R2_H)
 
   mode <- function(data) {
@@ -12,22 +12,27 @@ summary.ICA.BinCont <- function(object, ..., Object){
     mode_val <- z$x[which.max(z$y)]
     fit <- list(mode_val= mode_val)
   }
-  
+
   cat("\nFunction call:\n\n")
   print(Object$Call)
   cat("\n# Total number of valid R2_H values")
   cat("\n#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
   cat(length(Object$R2_H))
-  
+
   cat("\n\n\n# R2_H results summary")
-  cat("\n#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")  
-  cat("Mean (SD) R2_H: ", format(round(mean(Object$R2_H), 4), nsmall = 4), " (", format(round(sd(Object$R2_H), 4), nsmall = 4), ")") 
+  cat("\n#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+  cat("Mean (SD) R2_H: ",
+      format(round(mean(Object$R2_H), 4), nsmall = 4),
+      " (",
+      format(round(sd(Object$R2_H), 4), nsmall = 4),
+      ")")
   cat("\nMode R2_H: ", format(round(mode(Object$R2_H)$mode_val, 4), nsmall = 4))
-  cat("\nMedian R2_H: ", format(round(quantile(Object$R2_H), probs=0.50), nsmall = 4))
+  cat("\nMedian R2_H: ", format(round(quantile(Object$R2_H, probs = 0.50)), nsmall = 4))
   cat("\nRange R2_H: ", format(round(range(Object$R2_H), 4), nsmall = 4))
   cat("\n\nQuantiles of the R2_H distribution: \n\n")
-  quant <- quantile(Object$R2_H, probs = c(.05, .10, .20, .50, .80, .90, .95))
+  quant <-
+    quantile(Object$R2_H, probs = c(.05, .10, .20, .50, .80, .90, .95))
   print(quant)
 }
-  
+
 
