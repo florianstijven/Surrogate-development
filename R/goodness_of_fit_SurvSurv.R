c943b4ba56585861e7ec536b159e2b1bc2ed95d5
florianstijven <florian.stijven@gmail.com>
2024-01-25 13:50:01 +0100

    Update documentation

--- R/goodness_of_fit_SurvSurv.R
+++ R/goodness_of_fit_SurvSurv.R
@@ -61,6 +61,47 @@ marginal_gof_plots_scr = function(fitted_model,
 }
 
 
+#' Goodness-of-fit plot for the marginal survival functions
+#'
+#' The [marginal_gof_scr_S_plot()] and [marginal_gof_scr_T_plot()] functions
+#' plot the estimated marginal survival functions for the surrogate and true
+#' endpoints. In these plots, it is assumed that the copula model has been
+#' fitted for \eqn{(T_0, \tilde{S}_0, \tilde{S}_1, T_1)'} where \deqn{S_k =
+#' \min(\tilde{S_k}, T_k)} is the (composite) surrogate of interest. In these
+#' plots, the model-based survival functions for \eqn{(T_0, S_0, S_1, T_1)'} are
+#' plotted together with the corresponding Kaplan-Meier etimates.
+#'
+#' @param fitted_model Returned value from [fit_model_SurvSurv()]. This object
+#'   essentially contains the estimated identifiable part of the joint
+#'   distribution for the potential outcomes.
+#' @param grid Grid of time-points at which the model-based estimated regression
+#'   functions, survival functions, or probabilities are evaluated.
+#' @param treated (numeric) Treatment group. Should be `0` or `1`.
+#' @param ... Additional arguments to pass to [plot()].
+#'
+#' @details # True Endpoint
+#'
+#'   The marginal goodness-of-fit plots for the true endpoint, build by
+#'   [marginal_gof_scr_T_plot()], is simply a comparison of the model-based
+#'   estimate of \eqn{P(T_k > t)} with the Kaplan-Meier (KM) estimate obtained
+#'   with [survival::survfit()]. A pointwise 95% confidence interval for the KM
+#'   estimate is also plotted.
+#'
+#'   # Surrogate Endpoint
+#'
+#'   The model-based estimate of \eqn{P(S_k > s)} follows indirectly from the
+#'   fitted copula model because the copula model has been fitted for
+#'   \eqn{\tilde{S}_k} instead of \eqn{S_k}. However, the model-based estimate
+#'   still follows easily from the copula model as follows,
+#'   \deqn{P(S_k > s) = P(\min(\tilde{S}_k, T_k)) = P(\tilde{S}_k > s, T_k > s).}
+#'
+#'   The [marginal_gof_scr_T_plot()] function plots the model-based estimate for
+#'   \eqn{P(\tilde{S}_k > s, T_k > s)} together with the KM estimate (see above).
+#'
+#'
+#' @return `NULL`
+#' @export
+#' @inherit mean_S_before_T_plot_scr examples
 marginal_gof_scr_S_plot = function(fitted_model,
                                    grid,
                                    treated,
@@ -114,6 +155,8 @@ marginal_gof_scr_S_plot = function(fitted_model,
   )
 }
 
+#' @rdname marginal_gof_scr_S_plot
+#' @export
 marginal_gof_scr_T_plot = function(fitted_model,
                                    grid,
                                    treated,
@@ -182,6 +225,80 @@ mean_S_before_T_plots_scr = function(fitted_model,
 
 }
 
+#' Goodness of fit plot for the fitted copula
+#'
+#' The [mean_S_before_T_plot_scr()] and [prob_dying_without_progression_plot()]
+#' functions build plots to assess the goodness-of-fit of the copula model
+#' fitted by [fit_model_SurvSurv()]. Specifically, these two functions focus on
+#' the appropriateness of the copula. Note that to assess the appropriateness of
+#' the marginal functions, two other functions are available:
+#' [marginal_gof_scr_S_plot()] and [marginal_gof_scr_T_plot()].
+#'
+#' @param plot_method Defaults to `NULL`. Should not be modified.
+#' @inheritParams marginal_gof_scr_S_plot
+#'
+#' @details # Progression Before Death
+#'
+#' If a patient progresses before death, this means that \eqn{S_k < T_k}. For
+#' these patients, we can look at the expected progression time given that the
+#' patient has died at \eqn{T_k = t}: \deqn{E(S_k | T_k = t, S_k < T_k).} The
+#' [mean_S_before_T_plot_scr()] function plots the model-based estimate of this
+#' regression function together with a non-parametric estimate.
+#'
+#' This regression function can also be estimated non-parametrically by
+#' regressing \eqn{S_k} onto \eqn{T_k} in the subset of uncensored patients.
+#' This non-parametric estimate is obtained via `mgcv::gam(y~s(x))` with
+#' additionally `family = stats::quasi(link = "log", variance = "mu")` because
+#' this tends to describe survival data better. The 95% confidence intervals are
+#' added for this non-parametric estimate; although, they should be interpreted
+#' with caution because the Poisson mean-variance relation may be wrong.
+#'
+#' # Death Before Progression
+#'
+#' If a patient dies before progressing, this means that \eqn{S_k = T_k}. This
+#' probability can be modeled as a function of time, i.e., \deqn{	\pi_k(t) & =
+#' P(S_k = t  \, | \, T_k = t).} The [prob_dying_without_progression_plot()]
+#' function plots the model-based estimate of this regression function together
+#' with a non-parametric estimate.
+#'
+#' This regression function can also be estimated non-parametrically by
+#' regressing the censoring indicator for \eqn{S_k}, \eqn{\delta_{S_k}},
+#' onto \eqn{T_k} in the subset of patients with uncensored \eqn{T_k}.
+#'
+#' @return `NULL`
+#' @export
+#'
+#' @examples
+#' library(Surrogate)
+#' # Load Ovarian data
+#' data("Ovarian")
+#' # Recode the Ovarian data in the semi-competing risks format.
+#' data_scr = data.frame(
+#'   ttp = Ovarian$Pfs,
+#'   os = Ovarian$Surv,
+#'   treat = Ovarian$Treat,
+#'   ttp_ind = ifelse(
+#'     Ovarian$Pfs == Ovarian$Surv &
+#'       Ovarian$SurvInd == 1,
+#'     0,
+#'     Ovarian$PfsInd
+#'   ),
+#'   os_ind = Ovarian$SurvInd
+#' )
+#' # Fit copula model.
+#' fitted_model = fit_model_SurvSurv(data = data_scr,
+#'                                   copula_family = "clayton",
+#'                                   n_knots = 1)
+#' # Define grid for GoF plots.
+#' grid = seq(from = 1e-3,
+#'            to = 2.5,
+#'            length.out = 30)
+#' # Assess marginal goodness-of-fit in the control group.
+#' marginal_gof_scr_S_plot(fitted_model, grid = grid, treated = 0)
+#' marginal_gof_scr_T_plot(fitted_model, grid = grid, treated = 0)
+#' # Assess goodness-of-fit of the association structure, i.e., the copula.
+#' prob_dying_without_progression_plot(fitted_model, grid = grid, treated = 0)
+#' mean_S_before_T_plot_scr(fitted_model, grid = grid, treated = 0)
 mean_S_before_T_plot_scr = function(fitted_model,
                                     plot_method = NULL,
                                     grid,
@@ -338,6 +455,8 @@ prob_progression_before_dying = function(t, fitted_model, treated) {
   return(prob_progression_t)
 }
 
+#' @rdname mean_S_before_T_plot_scr
+#' @export
 prob_dying_without_progression_plot = function(fitted_model,
                                               plot_method = NULL,
                                               grid,
