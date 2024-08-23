2e9a9f54045972cd781b9ae4c243ab1e75ac2cfa
florianstijven <florian.stijven@gmail.com>
2024-08-23 14:50:55 +0200

    Update test for sensitivity_analysis_copula

--- tests/testthat/test-sensitivity_analysis_copula.R
+++ tests/testthat/test-sensitivity_analysis_copula.R
@@ -1,51 +1,13 @@
-test_that("multiplication works", {
-  # Fit model
-  S0 = c(2.2, 3.1, 0, -3, 0, 1, 4)
-  S0 = rep(S0, 10)
-  S1 = c(2, -0.5, 0, -3, 0, 2, 0)
-  S1 = rep(S1, 10)
-
-  T0 = c(1, 6, 2, 5, 3, 6, 4)
-  T0 = rep(T0, 10)
-  T1 = c(1, 5, 2, 6, 3, 6, 4)
-  T1 = rep(T1, 10)
-  K_T = 6
-
-  data = data.frame(
-    surrogate = c(S0, S1),
-    true = c(T0, T1),
-    treat = c(
-      rep(0, 70),
-      rep(1, 70)
-    )
-  )
-
-  marginal = list(
-    pdf_fun = function(x, para) {
-      dnorm(x, mean = para[1], sd = para[2])
-    },
-    cdf_fun = function(x, para) {
-      pnorm(x, mean = para[1], sd = para[2])
-    },
-    q_fun = function(p, para) {
-      qnorm(p, mean = para[1], sd = para[2])
-    },
-    n_para = 2
-  )
-
-  fitted_model = fit_copula_OrdCont(
-    data = data,
-    copula_family = "frank",
-    marginal_S0 = marginal,
-    marginal_S1 = marginal,
-    K_T = K_T,
-    start_copula = 2
-  )
+test_that("basic sensitivity analysis works for continuous-continuous, ordinal-continuous, and ordinal-ordinal", {
+  # Load fitted copula model.
+  fitted_model_contcont = readRDS(test_path("fixtures", "schizo-dvine-clayton-ContCont.rds"))
+  fitted_model_ordcont = readRDS(test_path("fixtures", "schizo-dvine-clayton-OrdCont.rds"))
+  fitted_model_ordord = readRDS(test_path("fixtures", "schizo-dvine-gaussian-OrdOrd.rds"))
 
   # Perform sensitivity analysis
   set.seed(1)
-  results = sensitivity_analysis_copula(
-    fitted_model = fitted_model,
+  results_contcont = sensitivity_analysis_copula(
+    fitted_model = fitted_model_contcont,
     n_sim = 5,
     eq_cond_association = FALSE,
     lower = rep(0.2, 4),
@@ -53,29 +15,91 @@ test_that("multiplication works", {
     degrees = 0,
     marg_association = TRUE,
     copula_family2 = "clayton",
-    n_prec = 5e3,
+    n_prec = 2e3,
+    ncores = 1
+  )
+  set.seed(1)
+  results_ordcont = sensitivity_analysis_copula(
+    fitted_model = fitted_model_ordcont,
+    n_sim = 5,
+    eq_cond_association = TRUE,
+    lower = rep(0.2, 4),
+    upper = rep(0.8, 4),
+    degrees = 0,
+    marg_association = FALSE,
+    copula_family2 = "frank",
+    n_prec = 2e3,
+    ncores = 1
+  )
+  set.seed(1)
+  results_ordord = sensitivity_analysis_copula(
+    fitted_model = fitted_model_ordord,
+    n_sim = 5,
+    eq_cond_association = FALSE,
+    lower = rep(0.2, 4),
+    upper = rep(0.8, 4),
+    degrees = 0,
+    marg_association = TRUE,
+    copula_family2 = "gumbel",
+    n_prec = 2e3,
     ncores = 1
   )
   # Correct ICA
+  {
   expect_equal(
-    results[, 1],
+    results_contcont[, 1],
     c(
-      0.0825988143900,
-      0.0821616252617,
-      0.1136904411633,
-      0.0488619958643,
-      0.1155981371652
+      0.805547826084,
+      0.804501092570,
+      0.828990437005,
+      0.362778037076,
+      0.849093738971
     )
   )
-  # Correct Spearman's rho
   expect_equal(
-    results[, 2],
+    as.numeric(results_contcont[1, ]),
+    c(0.805547826084, 0.864972741243, 0.909834719459, 0.591356631839, 0.691666788417, 0.361532829383, 0.450635314659,
+      0.923559431890, 6.105601327913, 0.649720879856, 6.596600115926, 2.467916678980, 0.563217212642, 1.070967086009,
+      0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000
+    ),
+    ignore_attr = "names"
+  )
+
+  expect_equal(
+    results_ordcont[, 1],
     c(
-      -0.547959472635,
-      -0.540196293513,
-      -0.608194702531,
-      -0.395349607610,
-      -0.614067609887
+      0.1416477223515,
+      0.1371087549909,
+      0.0353045132874,
+      0.0120349874214,
+      0.0843796516144
     )
   )
+  expect_equal(
+    as.numeric(results_ordcont[1, ]),
+    c(0.141647722351, 0.435308056965,             NA,             NA,             NA,             NA,             NA,
+      NA, 3.142968155547, 2.303975808732, 4.017478729159, 2.047511165423, 2.047511165423, 3.433422135475,
+      0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000
+    ),
+    ignore_attr = "names"
+  )
+  expect_equal(
+    results_ordord[, 1],
+    c(
+      0.192161887674,
+      0.194310196872,
+      0.221638256151,
+      0.158499048460,
+      0.198650867242
+    )
+  )
+  expect_equal(
+    as.numeric(results_contcont[1, ]),
+    c(0.805547826084, 0.864972741243, 0.909834719459, 0.591356631839, 0.691666788417, 0.361532829383, 0.450635314659,
+      0.923559431890, 6.105601327913, 0.649720879856, 6.596600115926, 2.467916678980, 0.563217212642, 1.070967086009,
+      0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000, 0.000000000000
+    ),
+    ignore_attr = "names"
+  )
+  }
 })
